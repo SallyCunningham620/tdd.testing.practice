4039221091030a0a20d7abe18558ee0d87d35c27
Sally <143855164+SallyCunningham620@users.noreply.github.com>
2026-01-21 11:01:36 -0600

    add caesar cipher function and testing

--- function.js
+++ function.js
@@ -13,3 +13,34 @@ export const calculator = {
   divide: (a, b) => a / b,
   multiply: (a, b) => a * b,
 };
+
+function shiftChar(char, shift) {
+  const index = char.charCodeAt(0);
+  let newIndex;
+
+  //Upper case letters(A-Z)
+  if (index >= 65 && index <= 90) {
+    newIndex = ((index - 65 + shift) % 26) + 65;
+  }
+  //Lower case letters(a-z) 
+  else if (index >= 97 && index <= 122) {
+    newIndex = ((index - 97 + shift) % 26) + 97;
+  }
+  //Non-alphabetic Characters
+  else {
+    return char;
+  }
+  
+  return String.fromCharCode(newIndex);
+};
+
+export function caesarCipher(str, shift) {
+  const correctShift = (shift % 26 + 26) % 26;
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    result += shiftChar(str[i], correctShift);
+  }
+
+  return result;
+}
--- function.test.js
+++ function.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString, calculator } from './function.js';
+import { capitalize, reverseString, calculator, caesarCipher } from './function.js';
 
 test('capitalize first character of a string', () => {
   expect(capitalize('hello')).toBe('Hello');
@@ -30,3 +30,24 @@ describe('calculator', () => {
     expect(calculator.multiply(2, 3)).toBe(6);
   });
 });
+
+describe('caesarCipher', () => {
+  test('shift letters correctly with wrapping', () => {
+    expect(caesarCipher('xyz', 3)).toBe('abc');
+    expect(caesarCipher('abc', 4)).toBe('efg');
+  });
+  test('preserve upper and lower case letters', () => {
+    expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');  
+    expect(caesarCipher('fISh', 4)).toBe('jMWl');
+  });
+  test('non-alphabetical characters remain unchanged', () => {
+    expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
+    expect(caesarCipher('It\'s a secret miSSion?', 5)).toBe('Ny\'x f xjhwjy rnXXnts?');
+  });
+  test('0 shift is unchanged', () => {
+    expect(caesarCipher('NoChange', 0)).toBe('NoChange');
+  });
+  test('Large shifts', () => {
+    expect(caesarCipher('abc', 40)).toBe('opq');
+  })
+})
